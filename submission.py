a8140b4bbd77344617bda9bc1bcc298593c24d2b
Unknown <norgrin123@gmail.com>
2019-12-19 11:54:22 +0200

    implemented minmax agent

--- submission.py
+++ submission.py
@@ -1,10 +1,14 @@
+import math
+
 from environment import Player, GameState, GameAction, get_next_state, SnakeAgentsList
 from utils import get_fitness
 import numpy as np
 from enum import Enum
 
+
 def manhaten_dist(p1: tuple, p2: tuple):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
 
 def heuristic(state: GameState, player_index: int) -> float:
     """
@@ -16,15 +20,13 @@ def heuristic(state: GameState, player_index: int) -> float:
     """
     # Insert your code here...
     snake_length = state.snakes[player_index].length
-    if not state.snakes[player_index].alive:
+    if not state.snakes[player_index].alive or len(state.living_agents) <= 1:
         return snake_length
 
     snakes: SnakeAgentsList = state.snakes
     fruits_locations = state.fruits_locations
     dists = [[manhaten_dist(fruit, snake.head) for fruit in fruits_locations] for snake in snakes]
-    return 1/(1+min(dists[player_index])) + snake_length
-
-
+    return 1 / (1 + min(dists[player_index])) + snake_length
 
 
 class MinimaxAgent(Player):
@@ -35,6 +37,7 @@ class MinimaxAgent(Player):
     hint: use the 'agent_action' property to determine if it's the agents turn or the opponents' turn. You can pass
     'None' value (without quotes) to indicate that your agent haven't picked an action yet.
     """
+
     class Turn(Enum):
         AGENT_TURN = 'AGENT_TURN'
         OPPONENTS_TURN = 'OPPONENTS_TURN'
@@ -44,6 +47,7 @@ class MinimaxAgent(Player):
         This class is a wrapper class for a GameState. It holds the action of our agent as well, so we can model turns
         in the game (set agent_action=None to indicate that our agent has yet to pick an action).
         """
+
         def __init__(self, game_state: GameState, agent_action: GameAction):
             self.game_state = game_state
             self.agent_action = agent_action
@@ -54,7 +58,46 @@ class MinimaxAgent(Player):
 
     def get_action(self, state: GameState) -> GameAction:
         # Insert your code here...
-        pass
+        search_depth = 2
+        val, action = self.minmax(MinimaxAgent.TurnBasedGameState(state, None), search_depth, self.player_index)
+
+        return action
+
+    """
+    return tuple(float, GameAction) where float is the minmax value
+    """
+
+    def minmax(self, state: TurnBasedGameState, depth: int, player_index: int):
+        if depth == 0 or state.game_state.is_terminal_state or not state.game_state.snakes[player_index].alive:
+            return heuristic(state.game_state, player_index), state.agent_action
+
+        if state.turn == MinimaxAgent.Turn.AGENT_TURN:
+            succ = [self.minmax(
+                MinimaxAgent.TurnBasedGameState(state.game_state, action),
+                depth, player_index)
+                for action in state.game_state.get_possible_actions(player_index)]
+
+            best_val = -math.inf
+            best_action: GameAction
+            for s in succ:
+                if best_val < s[0]:
+                    best_val = s[0]
+                    best_action = s[1]
+            return best_val, best_action
+        else:
+            succ = [self.minmax(
+                MinimaxAgent.TurnBasedGameState(
+                    get_next_state(state.game_state, {**opponents_actions, **{player_index: state.agent_action}}),
+                    None),
+                depth - 1, player_index)
+                for opponents_actions in
+                state.game_state.get_possible_actions_dicts_given_action(state.agent_action,
+                                                                         player_index=self.player_index)]
+            best_val = math.inf
+            for s in succ:
+                if best_val > s[0]:
+                    best_val = s[0]
+            return best_val, state.agent_action
 
 
 class AlphaBetaAgent(MinimaxAgent):
@@ -104,4 +147,3 @@ class TournamentAgent(Player):
 if __name__ == '__main__':
     SAHC_sideways()
     local_search()
-
